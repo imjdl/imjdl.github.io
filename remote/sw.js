6faf7121008a69310c73ec9913dbb8d1745aca12
imjdl <imjdl@gmail.com>
2023-12-01 13:56:43 +0800

    更新博客

--- remote/sw.js
+++ remote/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701397195';
+const cacheName = 'chirpy-1701399126';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701399126';
+const cacheName = 'chirpy-1701410202';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
