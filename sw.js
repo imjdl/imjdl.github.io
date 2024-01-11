d5061a9c373da223401a332be661b17a313a90f8
imjdl <imjdl@gmail.com>
2024-01-11 12:09:54 +0800

    add new post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704943530';
+const cacheName = 'chirpy-1704946130';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
